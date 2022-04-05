aa0ed5aabceac5355a42280856aa0c6a014f4cfc
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-04-05 17:46:02 -0500

    Creando Routing y Controllers para citas
    - Se detalla la nueva ruta en el index.php para las citas.
    - Se crea un nuevo CitaController con el metodo index.
    - Se crea una nueva carpeta llamada cita y adentro la vista index.php. Aqui estarán los servicios que el cliente desea agendar.

--- public/index.php
+++ public/index.php
@@ -4,17 +4,18 @@ require_once __DIR__ . '/../includes/app.php';
 
 
 use MVC\Router;
+
 use Controllers\LoginController;
+use Controllers\CitaController;
 
 
 $router = new Router();
 
-
+// ---------------------Para el Login-------------------------------------------- 
 // Iniciar Sesion
 $router->get('/', [LoginController::class, 'login']);
 $router->post('/', [LoginController::class, 'login']);
 $router->get('/logout', [LoginController::class, 'logout']);
-
 
 //Recuperar Password
 $router->get('/forget', [LoginController::class, 'forget']);
@@ -29,6 +30,13 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 // Confirmar cuenta
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
+
+
+
+// ---------------------Para las Citas-------------------------------------------- 
+// AREA PRIVADA
+$router->get('/cita', [CitaController::class, 'index']);
+
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
