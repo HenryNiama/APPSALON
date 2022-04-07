f229929dc5c815f5ca84deb83e1648663b740ce3
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-04-07 17:19:08 -0500

    Cambiando la seccion segun el botón presionado en el tab
    - Se agrega la funcion mostrarSeccion en el app.js

--- src/js/app.js
+++ src/js/app.js
@@ -16,12 +16,25 @@ function tabs() {
     botones.forEach(boton => {
         boton.addEventListener('click', function (e) {
             paso = parseInt(e.target.dataset.paso);
-            mostrarSeccion(paso);
+            mostrarSeccion();
         });
     });
 
 }
 
-function mostrarSeccion(paso) {
-    console.log('Mostrando seccion ' + paso);
+function mostrarSeccion() {
+
+    // Ocultar la seccion que tenga la clase de mostrar
+    const seccionAnterior = document.querySelector('.mostrar');
+    
+    if (seccionAnterior) { // Si existe alguna seccion que tenga la clase .mostrar, entonces:
+        seccionAnterior.classList.remove('mostrar');  
+    }
+
+
+    // Seleccionar la seccion con el paso:
+    const pasoSelector = `#paso-${paso}`; // Esta como paso-# en cada div.
+    const seccion = document.querySelector(pasoSelector);
+    seccion.classList.add('mostrar');
+
 }
