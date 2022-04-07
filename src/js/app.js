d279290bcb66eb6baaa363222ecdc0dd78df7806
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-04-07 17:27:32 -0500

    Finalizando la navegacion por tabs
    - Se corrige para que al carga la pagina aparezca ya la seccion de servicios.
    - Se resalta y quita la clase .actual de cada boton dentro del tab cuando se le da click.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 let paso = 1;
 
 document.addEventListener('DOMContentLoaded', function () {
+    mostrarSeccion(); // Llamo la funcion para que por defecto se muestre una seccion al cargar la pagina
     iniciarApp();
 });
 
@@ -16,7 +17,7 @@ function tabs() {
     botones.forEach(boton => {
         boton.addEventListener('click', function (e) {
             paso = parseInt(e.target.dataset.paso);
-            mostrarSeccion();
+            mostrarSeccion(); // Muestra y oculta las secciones
         });
     });
 
@@ -26,15 +27,24 @@ function mostrarSeccion() {
 
     // Ocultar la seccion que tenga la clase de mostrar
     const seccionAnterior = document.querySelector('.mostrar');
-    
+
     if (seccionAnterior) { // Si existe alguna seccion que tenga la clase .mostrar, entonces:
         seccionAnterior.classList.remove('mostrar');  
     }
-
 
     // Seleccionar la seccion con el paso:
     const pasoSelector = `#paso-${paso}`; // Esta como paso-# en cada div.
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
 
+
+    //  Quitar la clase de  '.actual' al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    // Resalta el tab Actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
