ded403ad4957899a147522e56053cf3fd11f4ce8
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-04-21 14:45:00 -0500

    Guardando los Servicios de la Cita
    - Se crea nuevo modelo CitaServicio
    - Se reescribe el metodo guardar del APIController el cual guarda los servicios isntanciando CitaServicio.

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use Model\Servicio;
 use Model\Cita;
+use Model\CitaServicio;
 
 class APIController{
     
@@ -18,11 +19,31 @@ class APIController{
 
     public static function guardar(){
         
+        // Almacena la Cita y devuevle el ID de la misma
         $cita = new Cita($_POST);
+        // Escribimos en la Tabla de Cita de la Base de Datos
+        $resultado = $cita->guardar(); // Retorna un array con el boolean y el Id de la Cita.
 
-        $resultado = $cita->guardar();
-     
-        echo json_encode($resultado);
+        // Almacena la Cita y el servicio(s)
+        $idCita = $resultado['id'];
+
+        // Voy a tomar un string, lo separo por comas, y lo convierto en un arreglo
+        $idServicios = explode(",", $_POST['servicios']); // Devuelve un array
+
+        // Almacena cada servicio con el id de la Cita.
+        foreach ($idServicios as $idServicio) {
+            $args = [
+                'citaId' => $idCita,
+                'servicioId' => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+
+        // Retornamos una respuesta
+        echo json_encode(['resultado' => $resultado]);
+
+
     }
 
 }
