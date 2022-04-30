c6ffeaad23163d472c19834d4df2859bc176a73f
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-04-29 20:43:43 -0500

    Creando el formulario para servicios
    - Se agrega el formulario.php en crear.php
    - Se escribe el formulario.php

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -3,3 +3,12 @@
 <p class="descripcion-pagina">Llena todos los campos para añadir un nuevo Servicio</p>
 
 <?php include_once __DIR__ . '/../templates/barra.php'; ?>
+
+<form action="/servicios/crear" method="POST" class="formulario">
+<!-- Voy a agregar el archivo formulario.php aqui. Esto dos archivos comparten solamente campos, pero
+NO comparten el action, ni tampoco el input de tipo SUBMIT de este archivo de crear.php -->
+
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+
+    <input type="submit" value="Guardar Nuevo Servicio" class="boton">
+</form>
