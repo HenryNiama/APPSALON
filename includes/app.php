5925822e4e752969c145b7f2cd20e02be2ce407c
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-05-03 18:44:45 -0500

    Creación de variables de entorno
    - Se debe instalar composer require vlucas/phpdotenv
    - Se crea el archivo .env dentro de la carpeta de includes.
    - Se detalla las caracteristicas de nuestra base de datos en este archivo.
    - Se agrega una nueva variable y se usa el metodo de la clase Dotenv que recién se instalo en el archivo de app.js

--- includes/app.php
+++ includes/app.php
@@ -1,9 +1,17 @@
 <?php 
 
+// Este es el archivo que arranca todo.
+
+// los archivos relacionados a composer, etc, deben ir primero, antes del database.php
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // carga automaticamente el archivo .env
+$dotenv->safeLoad(); // Indica que si el archivo no existe (.env), no nos va a marcar un error.
+
 require 'funciones.php';
 require 'database.php';
-require __DIR__ . '/../vendor/autoload.php';
+
 
 // Conectarnos a la base de datos
+
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,29 @@ class ComposerAutoloaderInit887c4668896d754bdd696b7dffe55547
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit887c4668896d754bdd696b7dffe55547::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire887c4668896d754bdd696b7dffe55547($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire887c4668896d754bdd696b7dffe55547($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
